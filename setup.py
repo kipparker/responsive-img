2892315f96846e3d20a39a4be95404de95eef70c
Kip Parker <kip@kipparker.co.uk>
2015-05-19 09:42:20 +0100

    Bumps release version to 0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='responsive-img',
-    version='0.1',
+    version='0.9',
     packages=['responsive'],
     include_package_data=True,
     license='MIT License',  # example license
